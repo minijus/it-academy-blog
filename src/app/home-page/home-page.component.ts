934b9bd474427d71b9583b8c20e44a7fc1b70df5
Jurgis Bridzius <jurgis@bridzius.lt>
2020-02-18 22:58:40 +0200

    Simplified components before lecture

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from "@angular/core";
-import {Post} from "../shared/post";
+import { Component, OnInit } from "@angular/core";
+import { Post } from "../shared/post";
+import { postList } from "../shared/post-list";
 
 @Component({
   selector: "app-home-page",
@@ -11,62 +12,7 @@ export class HomePageComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.posts = [
-      {
-        id: "0",
-        author: "Darius",
-        title: "Post 1",
-        content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Autem commodi cupiditate dolorem doloremque dolorum fugiat
-              saepe sint soluta ullam voluptates voluptatum?`,
-        img: "assets/photo-0.jpeg"
-      },
-      {
-        id: "1",
-        author: "Kazimieras",
-        title: "Post 2",
-        content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Autem commodi cupiditate dolorem doloremque dolorum fugiat
-              saepe sint soluta ullam voluptates voluptatum?`,
-        img: "assets/photo-1.jpeg"
-      },
-      {
-        id: "2",
-        author: "Indrė",
-        title: "Post 3",
-        content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Autem commodi cupiditate dolorem doloremque dolorum fugiat
-              saepe sint soluta ullam voluptates voluptatum?`,
-        img: "assets/photo-2.jpeg"
-      },
-      {
-        id: "3",
-        author: "Jurgis",
-        title: "Post 4",
-        content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Autem commodi cupiditate dolorem doloremque dolorum fugiat
-              saepe sint soluta ullam voluptates voluptatum?`,
-        img: "assets/photo-3.jpeg"
-      },
-      {
-        id: "4",
-        author: "Laura",
-        title: "Post 5",
-        content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Autem commodi cupiditate dolorem doloremque dolorum fugiat
-              saepe sint soluta ullam voluptates voluptatum?`,
-        img: "assets/photo-4.jpeg"
-      },
-      {
-        id: "5",
-        author: "Minijus",
-        title: "Post 6",
-        content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Autem commodi cupiditate dolorem doloremque dolorum fugiat
-              saepe sint soluta ullam voluptates voluptatum?`,
-        img: "assets/photo-5.jpeg"
-      }
-    ];
+    this.posts = postList;
   }
 
   handlePostReadMoreClick(post: Post) {
